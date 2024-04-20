bb72784c9c1fe75faf0c4fd2f727daa2a34d6423
DiamabuDev <diana.maldonado130@gmail.com>
2024-04-20 01:26:33 -0400

    responsive cards

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -30,7 +30,7 @@ const CardSection: React.FC<CardSectionProps> = ({ sectionTitle, cardsData }) =>
                 ))}
 
             </div>
-            <div className="flex justify-center gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {cardsData.map((card, index) => (
                     <Card
                         key={index}
